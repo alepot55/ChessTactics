e6cd3506aea0bed47d5127bdf7f5291c53a26743
Alessandro Potenza <ap.alepot@gmail.com>
2024-04-04 16:58:01 +0200

    scacchiera che si muove, aggiornato js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-const scacchiera = new Chessboard2('scacchiera', 'start');
+var board = Chessboard2('scacchiera', 'start')
+
+function onDragStart (source, piece, position, orientation) {
+  if (game.in_checkmate() === true || game.in_draw() === true ||
+      piece.search(/^b/) !== -1) {
+    return false
+  }
+}
+
+function onDrop (source, target) {
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q'
+  })
+
+  if (move === null) {
+    return 'snapback'
+  }
+}
+
+function onSnapEnd () {
+  board.position(game.fen())
+}
+
+board = Chessboard2('scacchiera', {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+})
---
+++ server.php
@@ -0,0 +1,6 @@
+<?php
+// Inserisci qui la tua logica per generare o recuperare la stringa FEN
+$fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+echo $fen;
+?>
