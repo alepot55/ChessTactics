29a2a4a31faee36e2766e1cfcb15e76cc6522552
Alessandro Potenza <ap.alepot@gmail.com>
2024-04-05 22:02:28 +0200

    messo problema risolvibile

--- js/problemi.js
+++ js/problemi.js
@@ -1,53 +1,81 @@
-// Qui gli id dei bottoni "aggiorna"
-var bottoni_aggiorna = ['aggiorna0', 'aggiorna1', 'aggiorna2', 'aggiorna3'];
+// Colonne problemi: PuzzleId,FEN,Moves,Rating,RatingDeviation,Popularity,NbPlays,Themes,GameUrl,OpeningTags
+var indice = 0;
+var sol = null;
 
+board = Chessboard2('problema', 'start');
 
-var indici = Array(length(bottoni_aggiorna)).fill(0);
-
-// Funzione per impostare un problema_i con un fen
-function aggiornaScacchiera(i, fen) {
-    game = new Chess(fen);
-    board = Chessboard2('problema_' + i, {
-        position: fen,
-    });
+// Funzione per aggiornare la scacchiera con una nuova FEN
+function aggiornaScacchiera(problema) {
+    const config = {
+        draggable: true,
+        position: problema[1],
+        onDrop: controllaMossa,
+    }
+    board = Chessboard2('problema', config);
+    sol = problema[2];
 }
 
-function setProblema(index, num_problema) {
-    // Crea una nuova richiesta AJAX
+// Funzione per creare una nuova richiesta AJAX
+function creaRichiestaAjax(url, metodo) {
     var xhr = new XMLHttpRequest();
-
-    // Configura la richiesta come GET alla URL del tuo script PHP
-    xhr.open('GET', 'http://localhost:3000/server.php?problema_index=' + index, true);
-
-    // Imposta l'intestazione della richiesta per accettare dati JSON
+    xhr.open(metodo, url, true);
     xhr.setRequestHeader('Accept', 'application/json');
+    return xhr;
+}
 
-    // Gestisci la risposta
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            // Parse the JSON response
-            var problema = JSON.parse(xhr.responseText);
+// Funzione per gestire la risposta di una richiesta di caricamento problema
+function gestisciRispostaProblema(xhr) {
+    if (xhr.status === 200) {
+        var problema = JSON.parse(xhr.responseText);
+        console.log(problema);
+        aggiornaScacchiera(problema);
+    }
+}
 
-            aggiornaScacchiera(num_problema, problema[1]);
-        }
+// Funzione per caricare un problema
+function caricaProblema(i) {
+    var xhr = creaRichiestaAjax('http://localhost:3000/server.php?problema_index=' + i, 'GET');
+
+    xhr.onload = function () {
+        gestisciRispostaProblema(xhr);
     };
 
-    // Invia la richiesta
     xhr.send();
+
+    indice++;
+    document.getElementById('descrizione').textContent = 'Risolvi il problema!';
 }
 
-// per ogni bottone "Problema i" imposta il problema i
-for (var i = 0; i <= length(bottoni_aggiorna); i++) {
-    (function (i) {
-        // Imposta il problema iniziale
-        setProblema(indici[i], i);
-        indici[i]++;
-
-        // Aggiorna il problema quando il bottone viene premuto
-        document.getElementById(bottoni_aggiorna[i]).addEventListener('click', function () {
-            setProblema(indici[i], i);
-            indici[i]++;
-            console.log("Scacchiera " + i + "aggiornata con problema " + indici[i])
-        });
-    })(i);
+function controllaMossa(args) {
+    sol_src = sol.split(' ')[0].slice(0, 2)
+    sol_tar = sol.split(' ')[0].slice(2, 4)
+    if (args['source'] === sol_src && args['target'] === sol_tar) {
+        mossaCorretta()
+    } else {
+        mossaErrata()
+        return 'snapback'
+    }
 }
+
+function mossaErrata() {
+    document.getElementById('descrizione').textContent = 'Mossa errata!';
+}
+
+function vittoria() {
+    document.getElementById('descrizione').textContent = 'Complimenti hai completato il puzzle!';
+}
+
+function mossaCorretta() {
+    if (sol.length === 9) {
+        return vittoria()
+    }
+    document.getElementById('descrizione').textContent = 'Esatto, continua così!';
+    board.move(sol.slice(5, 7) + '-' + sol.slice(7, 9))
+    sol = sol.slice(10)
+}
+
+// Carica il problema iniziale 
+caricaProblema(indice);
+document.getElementById('aggiorna').addEventListener('click', function () {
+    caricaProblema(indice);
+});
--- server.php
+++ server.php
@@ -5,41 +5,12 @@ session_start();
 // Imposta l'intestazione Access-Control-Allow-Origin
 header('Access-Control-Allow-Origin: *');
 
-// Leggi i problemi da un file di testo
-$problemi = file('data/problemi/matto_in_2.txt');
-
-// Crea un array per memorizzare i problemi
-$listaProblemi = array();
-$count = 0;
-$num_problema = 0;
-
-// Elabora ogni problema
-foreach ($problemi as $problema) {
-
-    if ($count != 3 && $count != 4) {
-
-        if ($count == 0) {
-            $listaProblemi[$num_problema] = array();
-        }
-
-        $line = trim($problema);
-
-        // aggiungi la riga all'array num_problema
-        $listaProblemi[$num_problema][$count] = $line;
-    }
-
-    // Incrementa il contatore
-    $count++;
-    if ($count == 5) {
-        $count = 0;
-        $num_problema++;
-    }
-}
+$csv = array_map('str_getcsv', file('data\problemi\puzzles.csv'));
 
 // Ottieni l'indice del problema dalla richiesta GET, se esiste
 $problema_index = isset($_GET['problema_index']) ? intval($_GET['problema_index']) : 0;
 
 // Restituisci il problema corrente come JSON
 header('Content-Type: application/json');
-echo json_encode($listaProblemi[$problema_index]);
+echo json_encode($csv[$problema_index]);
 ?>
